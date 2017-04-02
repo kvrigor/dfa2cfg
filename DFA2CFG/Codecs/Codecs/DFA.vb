615d63b9ca2ccb0b316463cd8b17413f4bea43fc
paul.rigor <paul.rigor@IPO-L30004908>
2017-04-02 20:12:10 +0800

    Populated DFA properties and behavior.

--- DFA2CFG/Codecs/Codecs/DFA.vb
+++ DFA2CFG/Codecs/Codecs/DFA.vb
@@ -1,5 +1,5 @@
 ﻿''' <summary>
-''' Data structure for deterministic finite automaton (DFA).
+''' Data structure for deterministic finite automaton (DFA) objects.
 ''' </summary>
 ''' <remarks>
 ''' A deterministic finite automaton M is a 5-tuple, (Q, Σ, δ, q0, F), consisting of:
@@ -10,7 +10,265 @@
 '''  - Set of accept states [ F ⊆ Q ]
 ''' </remarks>
 Public Class DFA
-    Public Sub New()
+    Implements ICloneable
 
+    Protected _states As List(Of String)
+    Protected _inputSymbols As List(Of String)
+    Protected _transitions As List(Of TransFunc)
+    Protected _acceptStates As List(Of String)
+
+#Region "Properties"
+    ''' <summary>
+    ''' Returns a string array of all the states of the DFA.
+    ''' </summary>
+    ''' <returns></returns>
+    Public ReadOnly Property States As String()
+        Get
+            Return _states.ToArray()
+        End Get
+    End Property
+
+    ''' <summary>
+    ''' Returns a string array of the input symbols.
+    ''' </summary>
+    ''' <returns></returns>
+    Public ReadOnly Property InputSymbols As String()
+        Get
+            Return _inputSymbols.ToArray()
+        End Get
+    End Property
+
+    ''' <summary>
+    ''' Returns an array of all the transition functions.
+    ''' </summary>
+    ''' <returns></returns>
+    Public ReadOnly Property Transitions As TransFunc()
+        Get
+            Return _transitions.ToArray()
+        End Get
+    End Property
+
+    ''' <summary>
+    ''' Returns a string array of the accept states.
+    ''' </summary>
+    ''' <returns></returns>
+    Public ReadOnly Property AcceptStates As String()
+        Get
+            Return _acceptStates.ToArray()
+        End Get
+    End Property
+
+    ''' <summary>
+    ''' Gets/sets the start state.
+    ''' </summary>
+    Public Property StartState As String
+
+    ''' <summary>
+    ''' Gets/sets the name of the DFA.
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Name As String
+#End Region
+
+#Region "Constructors"
+    ''' <summary>
+    ''' Initializes an empty DFA.
+    ''' </summary>
+    Public Sub New()
+        _states = Nothing
+        _inputSymbols = Nothing
+        _transitions = Nothing
+        _StartState = Nothing
+        _acceptStates = Nothing
     End Sub
+
+    ''' <summary>
+    ''' Initializes a DFA with the specified parameters.
+    ''' </summary>
+    ''' <param name="q">String array of all DFA states.</param>
+    ''' <param name="sigma">String array of input symbols.</param>
+    ''' <param name="delta">Array of transition functions.</param>
+    ''' <param name="q0">Start state.</param>
+    ''' <param name="f">String array of accept states.</param>
+    ''' <param name="name">Name identifying the DFA.</param>
+    Public Sub New(ByVal q As String(), ByVal sigma As String(), ByVal delta As TransFunc(), ByVal q0 As String, ByVal f As String(), Optional ByVal name As String = "")
+        _states = q.ToList()
+        _inputSymbols = sigma.ToList()
+        _transitions = delta.ToList()
+        _StartState = q0
+        _acceptStates = f.ToList()
+        _Name = name
+    End Sub
+
+    ''' <summary>
+    ''' Initializes a DFA from an existing DFA object.
+    ''' </summary>
+    ''' <param name="dfa">Existing DFA object.</param>
+    Public Sub New(ByVal dfa As DFA)
+        _states = dfa.States.ToList()
+        _inputSymbols = dfa.InputSymbols.ToList()
+        _transitions = dfa.Transitions.ToList()
+        _StartState = dfa.StartState
+        _acceptStates = dfa.AcceptStates.ToList()
+        _Name = dfa.Name
+    End Sub
+#End Region
+
+#Region "Core functions"
+    ''' <summary>
+    ''' Checks whether the DFA is valid or not. A DFA is considered valid if it meets the following conditions: 
+    '''   1) Q is not null
+    '''   2) Start state is a member of Q (q0 ∈ Q)
+    '''   3) Set of accept states is a subset of Q (F ⊆ Q) 
+    '''   4) Has exactly one state for each possible combination of a state and an input symbol (Q x Σ → Q)
+    ''' </summary>
+    ''' <returns></returns>
+    Public Overridable ReadOnly Property IsValid() As Boolean
+        Get
+            If IsEmptySet(_states) OrElse IsEmptyStringSet(_states) Then Return False
+            If Not _states.Contains(_StartState) Then Return False
+            If Not IsEmptySet(_acceptStates) Then
+                For Each acceptState In _acceptStates
+                    If Not _states.Contains(acceptState) Then
+                        Return False
+                    End If
+                Next
+            End If
+            For Each state In _states
+                'TODO: Add code to check that each state maps exactly once to every input symbol
+            Next
+            Return True
+        End Get
+    End Property
+
+    ''' <summary>
+    ''' Adds a new transition function to the transition table.
+    ''' </summary>
+    ''' <param name="tf">The new TransFunc object to be added.</param>
+    Public Overridable Sub AddTransitions(ByVal tf As TransFunc)
+        If _transitions.Contains(tf) Then
+            Throw New ArgumentException($"The transition table already contains {tf.ToString()}.")
+        Else
+            _transitions.Add(tf)
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Add new transition functions to the transition table.
+    ''' </summary>
+    ''' <param name="tfArray">Array of new TransFunc objects to be added.</param>
+    Public Overridable Sub AddTransitions(ByVal tfArray() As TransFunc)
+        For Each tf In tfArray
+            AddTransitions(tf)
+        Next
+    End Sub
+
+    ''' <summary>
+    ''' Adds a new state to the set of states.
+    ''' </summary>
+    ''' <param name="s">The new state to be added.</param>
+    Public Overridable Sub AddStates(ByVal s As String)
+        If _states.Contains(s) Then
+            Throw New ArgumentException($"The set of states already contains '{s}'.")
+        Else
+            _states.Add(s)
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Adds new states to the set of states.
+    ''' </summary>
+    ''' <param name="sArray">String array of the new states to be added.</param>
+    Public Overridable Sub AddStates(ByVal sArray() As String)
+        For Each s In sArray
+            AddStates(s)
+        Next
+    End Sub
+
+    ''' <summary>
+    ''' Adds a new input symbol to the set of input symbols.
+    ''' </summary>
+    ''' <param name="s">The new input symbol to be added.</param>
+    Public Overridable Sub AddInputSymbols(ByVal s As String)
+        If _inputSymbols.Contains(s) Then
+            Throw New ArgumentException($"The set of input symbols already contains '{s}'.")
+        Else
+            _inputSymbols.Add(s)
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Adds new input symbols to the set of input symbols.
+    ''' </summary>
+    ''' <param name="sArray">String array of the new input symbols to be added.</param>
+    Public Overridable Sub AddInputSymbols(ByVal sArray() As String)
+        For Each s In sArray
+            AddInputSymbols(s)
+        Next
+    End Sub
+
+    ''' <summary>
+    ''' Adds a new final state to the set of final states.
+    ''' </summary>
+    ''' <param name="s">The new final state to be added.</param>
+    Public Overridable Sub AddFinalStates(ByVal s As String)
+        If _acceptStates.Contains(s) Then
+            Throw New ArgumentException($"The set of final states already contains '{s}'.")
+        Else
+            _acceptStates.Add(s)
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Adds new final states to the set of final states.
+    ''' </summary>
+    ''' <param name="sArray">String array of the new input symbols to be added.</param>
+    Public Overridable Sub AddFinalStates(ByVal sArray() As String)
+        For Each s In sArray
+            AddFinalStates(s)
+        Next
+    End Sub
+#End Region
+
+#Region "Object helper functions"
+    Public Overrides Function ToString() As String
+        Return $"DFA Name: {_Name}" & vbCrLf &
+               $"    Q = {{{String.Join(", ", _states)}}}" & vbCrLf &
+               $"    Σ = {{{String.Join(", ", _inputSymbols)}}}" & vbCrLf &
+               $"   q0 = {_StartState}" & vbCrLf &
+               $"    F = {{{String.Join(", ", _acceptStates)}}}" & vbCrLf &
+               "    𝛿:" & vbCrLf & PrintTransitionTable()
+    End Function
+
+    Public Function Clone() As Object Implements ICloneable.Clone
+        Return New DFA(States, InputSymbols, Transitions, StartState, AcceptStates)
+    End Function
+#End Region
+
+#Region "Private helper functions"
+    Protected Function PrintTransitionTable() As String
+        'TODO: Display transition functions in tabular format 
+        Return ""
+    End Function
+#End Region
+
+End Class
+
+Public Class TransFunc
+    Public Property PrevState As String
+    Public Property Input As String
+    Public Property NextState As String
+
+    Public Overrides Function Equals(obj As Object) As Boolean
+        If TypeOf obj Is TransFunc Then
+            Dim tfObj As TransFunc = CType(obj, TransFunc)
+            Return Me.PrevState = tfObj.PrevState AndAlso Me.Input = tfObj.Input AndAlso Me.NextState = tfObj.NextState
+        Else
+            Return False
+        End If
+    End Function
+
+    Public Overrides Function ToString() As String
+        Return $"𝛿({PrevState}, {Input}) = {NextState}"
+    End Function
 End Class
--- DFA2CFG/Codecs/Codecs/Utils.vb
+++ DFA2CFG/Codecs/Codecs/Utils.vb
@@ -1,3 +1,21 @@
 ﻿Module Utils
+    Public Const EMPTY_SET = "∅"
+    Public Const EMPTY_STRING = "ε"
 
+    Public Function IsEmptySet(ByVal setObj As IEnumerable(Of String)) As Boolean
+        Return setObj Is Nothing OrElse setObj.Count = 0
+    End Function
+
+    Public Function IsEmptyStringSet(ByVal setObj As IEnumerable(Of String)) As Boolean
+        If IsEmptySet(setObj) Then
+            Return False
+        Else
+            For Each element In setObj
+                If Not String.IsNullOrEmpty(element) AndAlso element <> EMPTY_STRING Then
+                    Return False
+                End If
+            Next
+            Return True
+        End If
+    End Function
 End Module
