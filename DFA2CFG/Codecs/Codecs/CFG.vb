f9746aaeb08b1470c7b4a98ce65e7584cd79b4d8
paul.rigor <paul.rigor@IPO-L30004908>
2017-04-02 13:19:07 +0800

    Added XML docs to CFG.vb

--- DFA2CFG/Codecs/Codecs/CFG.vb
+++ DFA2CFG/Codecs/Codecs/CFG.vb
@@ -1,3 +1,15 @@
-﻿Public Class CFG
+﻿''' <summary>
+''' Data structure for context free grammar (CFG) objects.
+''' </summary>
+''' <remarks>
+''' A context free grammar is a 4-tuple, (V, Σ, R, S), consisting of:
+'''  - Finite set of variables [ V ]
+'''  - Finite set of terminals disjoint from V [ Σ ] 
+'''  - Rules of the grammar (a.k.a productions) [ R: V → (V ∪ Σ)*]
+'''  - Start variable [ S ∈ V]
+''' </remarks>
+Public Class CFG
+    Public Sub New()
 
+    End Sub
 End Class
