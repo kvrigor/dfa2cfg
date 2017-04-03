7b0aff146771642112e322bdf4e4a07173661035
Chester <chesterlbtan@gmail.com>
2017-04-03 21:11:49 +0800

    GUI Initial Release

---
+++ AutomataGUI/AutomataGUI/Globals.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomataGUI
+{
+    public static class Globals
+    {
+        public static Point TargetZero;
+
+
+        public static MouseCondition MouseStatus;
+
+        public enum MouseCondition
+        {
+            Default,
+            Hovered,
+            AddState,
+            DeleteState,
+            MoveState,
+            ConnectZero,
+            ConnectOne
+        }
+    }
+}
